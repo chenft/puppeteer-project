14cb7a0ed4ae39e96743a50f3364d6d9bb9a63f2
chenft <chenfengting@weshineapp.com>
2019-12-23 19:40:44 +0800

    新增server酱消息推送

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const puppeteer = require('puppeteer');
 // const devices = require('puppeteer/DeviceDescriptors');
 const nodemailer = require('nodemailer'); //发送邮件的node插件
 // const iPhone = devices['iPhone X'];
-const argv = process.argv
+const qs = require('querystring');
+const argv = process.argv;
+const http = require("http");
+const url = require('url');
+const SCKEY = 'SCU49140T45a63036ab78914771cbecfaa66845eb5cb7ed1b474a5';
+
 if (argv.length <= 2) {
   console.log('请指定待处理的项目名称')
   return
@@ -60,37 +65,66 @@ if (!curUrl) {
     }
   }, 3000);
 
-  let transporter = nodemailer.createTransport({
-    service: 'QQ', // 发送者的邮箱厂商，支持列表：https://nodemailer.com/smtp/well-known/
-    port: 465, // SMTP 端口
-    secureConnection: true, // SSL安全链接
-    auth: {   //发送者的账户密码
-      user: '625027428@qq.com', //账户
-      pass: 'wrphlyzmgsafbbhh', //smtp授权码，到邮箱设置下获取
-    }
-  });
+  // let transporter = nodemailer.createTransport({
+  //   service: 'QQ', // 发送者的邮箱厂商，支持列表：https://nodemailer.com/smtp/well-known/
+  //   port: 465, // SMTP 端口
+  //   secureConnection: true, // SSL安全链接
+  //   auth: {   //发送者的账户密码
+  //     user: '625027428@qq.com', //账户
+  //     pass: 'wrphlyzmgsafbbhh', //smtp授权码，到邮箱设置下获取
+  //   }
+  // });
 
-  let mailOptions = {
-    from: 'chenft <625027428@qq.com>', // 发送者昵称和地址
-    to: '625027428@qq.com', // 接收者的邮箱地址
-    subject: 'puppeteer', // 邮件主题
-    html: `<div>${argv[2]} 构建成功！</div>`,
-    // attachments: [{
-    //   filename: 'mryx.jpg',
-    //   path: './mryx.jpg',
-    //   cid: '001'
-    // }]
-  };
+  // let mailOptions = {
+  //   from: 'chenft <625027428@qq.com>', // 发送者昵称和地址
+  //   to: '625027428@qq.com', // 接收者的邮箱地址
+  //   subject: 'puppeteer', // 邮件主题
+  //   html: `<div>${argv[2]} 构建成功！</div>`,
+  //   // attachments: [{
+  //   //   filename: 'mryx.jpg',
+  //   //   path: './mryx.jpg',
+  //   //   cid: '001'
+  //   // }]
+  // };
 
-  function buildSuccess () {
+  async function buildSuccess () {
     //发送邮件
-    transporter.sendMail(mailOptions, async (error, info) => {
+    // transporter.sendMail(mailOptions, async (error, info) => {
+    //   await page.close();
+    //   await browser.close();
+    //   if (error) {
+    //     return console.log(error);
+    //   }
+    //   console.log('邮件发送成功 ID：', info.messageId);
+    // });
+    var post_data = {
+      text: `<div>${argv[2]} 构建成功！</div>`,
+      desp: '噜啦噜啦嘞'
+    };
+    var content = qs.stringify(post_data);
+    var res = sendToWx(content);
+    res.then(async () => {
+      console.log('方糖发送成功');
       await page.close();
       await browser.close();
-      if (error) {
-        return console.log(error);
-      }
-      console.log('邮件发送成功 ID：', info.messageId);
+    }, async (err) => {
+      console.log('方糖发送失败');
+      await page.close();
+      await browser.close();
+    });
+  }
+
+  function sendToWx(content) {
+    return new Promise((resolve, reject) => {
+      http.get(`http://sc.ftqq.com/SCU49140T45a63036ab78914771cbecfaa66845eb5cb7ed1b474a5.send?${content}`, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', resolve);
+      })
     });
   }
 })();
+
+
